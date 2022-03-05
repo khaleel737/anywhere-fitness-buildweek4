0670adb171b38a36aa64a0726a898c61e4034bfc
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2022-03-04 18:12:14 -0800

    hey im alive

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const server = require('./api/server');
 const { PORT } = require('./api/secrets/index');
 
+   
+const express = require("express");
+const app = express();
+
+app.use(express.json({ extended: false }));
+
 server.listen(PORT, () => {
     console.log(`Server Is Live On Port: ${PORT}`);
 })
